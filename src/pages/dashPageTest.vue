0614abeff23284f683defc5c0b2c65a5c14811f5
Arsen Asanov <tatarin08082002@gmail.com>
2025-11-18 14:47:50 +0300

    добавил страницу результатов

--- src/pages/dashPageTest.vue
+++ src/pages/dashPageTest.vue
@@ -1,17 +1,22 @@
 <script setup>
-import InteractivePlate from '@/components/dash/InteractivePlate.vue';
-import { useStore } from 'vuex';
+import InteractivePlate from "@/components/dash/InteractivePlate.vue";
+import ResultPlate from "@/components/dash/ResultPlate.vue";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const resultInfo = ref(null);
 
 const store = useStore();
 const goResult = () => {
-    console.log('goResult');
-    store.dispatch("dashResult/calculateDash", 1);
-    const result = store.getters["dashResult/getResult"];
-
-    console.log('result', result);
-}
+  console.log("goResult");
+  store.dispatch("dashResult/calculateDash", 1);
+  const result = store.getters["dashResult/getResult"];
+  resultInfo.value = result;
+  // console.log("result", result);
+};
 </script>
 
 <template>
-    <InteractivePlate @goResult="goResult" />
+  <InteractivePlate @goResult="goResult" />
+  <ResultPlate v-if="resultInfo" :result-obj="resultInfo"/>
 </template>
